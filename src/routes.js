0ec8071f7bad205d8e20b748e8b155c0a5a32629
AndersonAlvesCoelho <a-a-coelho-@hotmail.com>
2021-03-18 01:06:40 -0300

    Route Auth

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './auth';
+
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        isAuthenticated() ? (
+            <Component {...props} />
+        ) : (
+            <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+        )
+    )}
+    />
+);
+
+
+const Routes = () => (
+
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={() => <h1>Hello Word</h1>} />
+            <PrivateRoute path="/app"component={() => <h1>Você está logado</h1>}  />
+        </Switch>
+    </BrowserRouter>
+);
+
+export default Routes;
